5f4340717e0125434ea01cb838b931fdc021d729
kim-dongho <61777992+kim-dongho@users.noreply.github.com>
2024-09-20 12:31:26 +0900

    [level 2] Title: [1차] 캐시, Time: 41.74 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,24 @@
+function solution(cacheSize, cities) {
+    var answer = 0;
+    let arr = [];
+    
+    if (cacheSize === 0) return cities.length * 5;
+    
+    for (let city of cities) {
+        city = city.toUpperCase();
+        
+        if(arr.includes(city)) {
+            let index = arr.indexOf(city);
+            arr.splice(index, 1);
+            arr.unshift(city);
+            answer += 1;
+        } else {
+            if(!(arr.length < cacheSize)) {
+                arr.pop();
+            }
+            arr.unshift(city);
+            answer += 5;
+        }
+    }
+    return answer;
+}
