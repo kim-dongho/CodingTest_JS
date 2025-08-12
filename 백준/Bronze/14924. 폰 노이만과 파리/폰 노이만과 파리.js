d25599dc4194f84642ecb0c2b511e2717dd96a5a
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-12 11:43:55 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 88 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.js
@@ -0,0 +1,22 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+const fs = require('fs');
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+// 3. 입력값이 여러 줄일 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split("\n");
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [S, T, D] = input;
+const ans = (D / (S*2)) * T
+console.log(ans);
