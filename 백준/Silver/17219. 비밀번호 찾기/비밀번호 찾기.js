8d36a18fcaa4f513ae1da37ed1e8141d43e568ec
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-12 17:01:39 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 3844 ms, Memory: 86404 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.js
@@ -0,0 +1,39 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(" ");
+// 3. 입력값이 여러 줄일 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split("\n");
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const fs = require('fs');
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+let [N, M] = input[0].split(' ');
+const sites = input.slice(1, Number(N)+1).map((item) => item.split(' '));
+const finds = input.slice(Number(N)+1);
+
+const solution = () => {
+	const map = new Map();
+
+	sites.forEach((value) => {
+		const [site, password] = value;
+		map.set(site, password);
+	});
+
+	finds.forEach((value) => {
+		if(map.has(value)) {
+			console.log(map.get(value));
+		}
+	})
+}
+
+solution();
