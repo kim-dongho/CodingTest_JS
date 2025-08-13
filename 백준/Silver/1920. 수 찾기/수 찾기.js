7ef53291c43bfd274cbf12bc8e04edb385169309
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-13 14:36:38 +0900

    [Silver IV] Title: 수 찾기, Time: 3640 ms, Memory: 66336 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -0,0 +1,37 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(" ");
+// 3. 입력값이 여러 줄일 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const fs = require('fs');
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const n = input[0];
+const array_n = input[1].split(' ').map((item) => Number(item));
+const m = input[2];
+const array_m = input[3].split(' ').map((item) => Number(item));
+
+const solution = () => {
+	const comp = new Set([...array_n]);
+
+	for(let i = 0; i < array_m.length; i++) {
+		if(comp.has(array_m[i])) {
+			console.log(1);
+		} else {
+			console.log(0);
+		}
+	}
+}
+
+solution();
