faaeffc495b8c473d1bbef5d38fb4514db8e7887
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-13 15:15:58 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 104 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -0,0 +1,45 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+
+// 1. 입력값이 한 개일 때(한 줄)
+const fs = require('fs');
+const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(" ");
+// 3. 입력값이 여러 줄일 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const N = input[0];
+
+
+const factorial = (n) => {
+	let result = 1n;
+
+	for(let i = 2n; i <= BigInt(n); i++) {
+		result *= i;
+	}
+
+	return result;
+}
+
+const solution = () => {
+	let count = 0;
+	const result = factorial(input);
+
+	String(result).split('').reverse().some((value) => {
+		if(value !== '0') return true;
+		count++;
+	})
+
+	console.log(count);
+}
+
+solution();
