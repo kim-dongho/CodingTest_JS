eca1629ab2400e0a6697f8647acf9480ada94d4d
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-18 14:50:44 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 9868 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
@@ -0,0 +1,51 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const fs = require('fs');
+// const [M, N] = fs.readFileSync(filePath).toString().trim().split(" ").map((item) => Number(item));
+// 3. 입력값이 여러 줄일 때
+// const fs = require('fs');
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const fs = require('fs');
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const fs = require('fs');
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const N = input[0];
+const array = input.slice(1).map((item) => {
+	return Number(item);
+});
+
+
+const solution = () => {
+	let arr = [];
+	
+	for(let i = 0; i <= 14; i++) {
+		arr[i] = [];
+		for(let j = 0; j <= 14; j++) {
+			if(i === 0) {
+				arr[i][j] = j+1;
+				continue;
+			}
+			if(j === 0) {
+				arr[i][j] = 1;
+				continue;
+			}
+
+			arr[i][j] = arr[i-1][j] + arr[i][j-1];
+		}
+	}
+
+	for (let i = 0; i < array.length; i += 2) {
+  	const [k, n] = array.slice(i, i + 2);
+		console.log(arr[k][n-1]);
+	}
+}	
+
+solution();
