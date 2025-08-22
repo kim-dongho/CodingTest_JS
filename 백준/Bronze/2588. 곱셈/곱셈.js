f0945c2c62a5f3b06e51bbe2fedcbc6f9ac1cd05
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-22 15:33:41 +0900

    [Bronze III] Title: 곱셈, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -0,0 +1,32 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+// const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(" ").map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const n = input[0];
+// const array = input.slice(1);
+
+const solution = () => {
+	const [A, B] = input;
+	const nA = Number(A);
+
+	B.split('').reverse().map((v) => {
+		const nv = Number(v);
+		console.log(nA * nv);
+	})
+
+	console.log(nA * Number(B));
+}
+
+solution();
