10ddf2fc83b497d787954949b31d7c0faf6b6397
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-22 17:01:30 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -0,0 +1,33 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+// const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(" ").map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const [N, M] = input[0].split(' ').map((v) => Number(v));
+const arr = input.slice(1);
+
+const solution = () => {
+	const ans = Array.from({length: N}, (_,i) => i+1);
+
+	arr.forEach((v) => {
+		const [i, j] = v.split(' ').map((v) => Number(v)-1);
+
+		const temp = ans.slice(i, j+1).reverse();
+		ans.splice(i, j-i+1, ...temp);
+	})
+	
+	console.log(...ans);
+}
+
+solution();
