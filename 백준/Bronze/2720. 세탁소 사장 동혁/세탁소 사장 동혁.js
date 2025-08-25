fe15686a9e8ab9d9072c54523ba077d7c1102a03
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-25 12:32:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 180 ms, Memory: 12124 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
@@ -0,0 +1,35 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(" ");
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const N = Number(input[0]);
+const array = input.slice(1).map((v) => Number(v));
+
+const solution = () => {
+	array.forEach((num) => {
+		let arr = [];
+		[25, 10, 5, 1].map((v) => {
+			if(num >= v) {
+				arr.push(Math.floor(num / v));
+				num %= v;
+			} else {
+				arr.push(0);
+			}
+		})
+		console.log(...arr);
+	})
+}
+
+solution();
