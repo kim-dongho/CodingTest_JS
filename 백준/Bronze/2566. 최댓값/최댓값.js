2c7016d8a240077956c7d456f883832694aeaf30
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-25 10:52:49 +0900

    [Bronze III] Title: 최댓값, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -0,0 +1,36 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(" ").map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split(splitKeyword);
+
+const solution = () => {
+	let max = 0;
+	let index = '1 1';
+
+	for(let i = 0; i < input.length; i++) {
+		input[i] = input[i].split(' ');
+		for(let j = 0; j < input[i].length; j++) {
+			const N = Number(input[i][j]);
+			if(N > max) {
+				max = N;
+				index = `${i+1} ${j+1}`;
+			}
+		}
+	}
+	console.log(max);
+	console.log(index);
+}
+
+solution();
