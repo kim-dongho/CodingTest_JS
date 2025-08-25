72179db0d6ffa3107bfbc9a9b5163e7f818305a0
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-25 11:23:59 +0900

    [Bronze I] Title: 세로읽기, Time: 112 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -0,0 +1,42 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(" ").map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword).map((v) => v.split(''));
+
+const solution = () => {
+	let max = 0;
+	for(let i = 0; i < input.length; i++) {
+		max = Math.max(input[i].length, max);
+	}
+
+	for(let i = 0; i < input.length; i++) {
+		if(input[i].length < max) {
+			const arr = Array.from({length: max - input[i].length}, () => {return undefined});
+			input[i] = [...input[i], ...arr];
+		}
+	}
+
+	let str = '';
+	for(let i = 0; i < max; i++) {
+		for(let j = 0; j < input.length; j++) {
+			if(input[j][i] !== undefined) {
+				str += input[j][i];
+			}
+		}
+	}
+	console.log(str);
+}
+
+solution();
