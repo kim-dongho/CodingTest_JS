b2cf08f2655de730ffa01e3cad03eee5bf3312ec
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-26 11:38:54 +0900

    [Bronze II] Title: 커트라인, Time: 96 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -0,0 +1,24 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(' ').map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const [N, K] = input[0].split(' ').map((v) => Number(v));
+const arr = input[1].split(' ').sort((a, b) => { return b - a }).map((v) => Number(v));
+
+const solution = () => {
+	console.log(arr[K-1]);
+}
+
+solution();
