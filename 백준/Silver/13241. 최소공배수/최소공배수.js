b5cc577df4b50d28f814dd37561fea0ca4c696fa
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-28 09:00:01 +0900

    [Silver V] Title: 최소공배수, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.js
@@ -0,0 +1,46 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const testcase = [0, 'utf-8'];
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(' ').map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+const [A, B] = input[0].split(' ').map((v) => BigInt(v));
+
+const gcd = (A, B) => {
+	let tmp, n;
+
+	if(A < B) {
+		tmp = A;
+		A = B;
+		B = tmp;
+	}
+
+	while(B != 0) {
+		n = A % B;
+		A = B;
+		B = n;
+	}
+
+	return A;
+}
+
+const lcm = (A, B) => {
+	return A * B / gcd(A,B);
+}
+
+const solution = () => {
+	const value = lcm(A, B);
+	console.log(value.toString());
+}
+
+solution();
