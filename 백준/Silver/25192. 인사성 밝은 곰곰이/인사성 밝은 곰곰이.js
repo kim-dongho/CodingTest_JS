164a49fab77cc82188602bb785b151723e8da2b8
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-29 11:51:09 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 38040 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.js
@@ -0,0 +1,40 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const isTest = false;
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(' ').map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs
+  .readFileSync(isTest ? 0 : filePath, 'utf-8')
+  .toString()
+  .trim()
+  .split(splitKeyword);
+
+const N = Number(input[0]);
+const str = input.slice(2).join(' ').trim();
+
+const solution = () => {
+	const arr = str.split('ENTER').map((v) => v.trim().split(' ').filter((v) => v !== ''));
+	let ans = 0;
+
+	for(let i = 0; i < arr.length; i++) {
+		if(!(arr[i].length)) continue;
+
+		const set = new Set(arr[i]);
+
+		ans += set.size;
+	}
+
+	console.log(ans);
+}
+
+solution();
