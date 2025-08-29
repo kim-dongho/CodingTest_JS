c7c6a4a2bd59da9b19b2240e1f29f6a87fe2895d
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-08-29 10:46:12 +0900

    [Bronze IV] Title: 베라의 패션, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,28 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const isTest = false;
+
+// 1. 입력값이 한 개일 때(한 줄)
+// const input = fs.readFileSync(filePath).toString().trim();
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+// const input = fs.readFileSync(filePath).toString().trim().split(' ').map((v) => Number(v));
+// 3. 입력값이 여러 줄일 때
+// const input = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// const [n, input] = fs.readFileSync(filePath).toString().trim().split(splitKeyword);
+// const inputArr = input.trim().split(" ")
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const input = fs
+  .readFileSync(isTest ? 0 : filePath, 'utf-8')
+  .toString()
+  .trim()
+  .split(splitKeyword);
+
+const N = Number(input[0]);
+
+const solution = () => {
+	console.log(N * (N-1));
+}
+
+solution();
