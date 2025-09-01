3cf88af2a9c86c03e3fa29d1815097a82f237574
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-09-01 12:04:14 +0900

    [Silver V] Title: 창문 닫기, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.js
@@ -0,0 +1,18 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const isTest = false;
+
+const input = fs
+  .readFileSync(isTest ? 0 : filePath, 'utf-8')
+  .toString()
+  .trim()
+  .split(splitKeyword);
+
+const N = Number(input[0]);
+
+const solution = () => {
+  console.log(Math.floor(Math.sqrt(N)));
+}
+
+solution();
