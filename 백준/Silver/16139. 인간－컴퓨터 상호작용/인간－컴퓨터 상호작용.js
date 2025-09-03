0f9b845ae384ff7e3da020d7045f46c92d30780e
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-09-03 11:02:57 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 524 ms, Memory: 116060 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -1,7 +1,7 @@
 const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
 const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
 const fs = require('fs');
-const isTest = true;
+const isTest = false;
 
 const input = fs
   .readFileSync(isTest ? 0 : filePath, 'utf-8')
@@ -9,32 +9,45 @@ const input = fs
   .trim()
   .split(splitKeyword);
 
-const S = input[0]
+const S = input[0].split('');
 const arr = input.slice(2);
 
 const solution = () => {
-  const map = new Map();
+  let str = '';
+  const alpha = Array.from(Array(26), () => new Array(S.length).fill(0));
 
-  S.split('').map((v, i) => {
-    if(map.has(v)) {
-      const value = [...map.get(v), i];
-      map.set(v, value);
-    } else {
-      map.set(v, [i]);
+  for(let i = 0; i < S.length; i++) {
+    const c = S[i].charCodeAt(0) - 97;
+    
+    if(i === 0) {
+      alpha[c][i] = 1;
+      continue;
     }
-  });
 
-  for(let i = 0; i < arr.length; i++) {
-    let count = 0;
-    const [ch, start, end] = arr[i].split(' ');
-    if(map.has(ch)) {
-      for(let j = Number(start); j <= Number(end); j++) {
-        const value = map.get(ch);
-        if(value.includes(j)) count++;
+    for(let j = 0; j < alpha.length; j++) {
+      if(c === j) {
+        alpha[j][i] = alpha[j][i-1] + 1; 
+      } else {
+        alpha[j][i] = alpha[j][i-1];
       }
     }
-    console.log(count);
   }
+
+  for(let i = 0; i < arr.length; i++) {
+    let [ch, start, end] = arr[i].split(' ').map((v, idx) => {
+      if (idx === 0) return v; 
+      return Number(v); 
+    });
+    ch = ch.charCodeAt(0) - 97;
+    if(start === 0) {
+      str += alpha[ch][end]; 
+    } else {
+      str += alpha[ch][end] - alpha[ch][start-1];
+    }
+    str += '\n';
+  }
+
+  console.log(str);
 }
 
 solution();
