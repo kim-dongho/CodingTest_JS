65e690c6b9e29af056e82ac53cd9fbb6d87f0407
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-09-09 15:27:49 +0900

    [Silver I] Title: IOIOI, Time: 196 ms, Memory: 33976 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -14,26 +14,22 @@ const M = Number(input[1]);
 const str = input[2].split('');
 
 const solution = () => {
-  const length = (N*2+1);
-  let io = '';
-  let cnt = 0;
+  let cnt = 0, ans = 0;
 
-  for(let i = 1; i <= length; i++) {
-    if(i % 2 === 0) {
-      io += 'O';
-    } else {
-      io += 'I';
-    }
-  }
-
-  for(let i = 0; i < M-length+1; i++) {
-    const chk = str.slice(i, i+length);
-    if(chk.join('') === io) {
+  for(let i = 0; i + 2 < M; i++) {
+    if(str[i] === 'I' && str[i+1] === 'O' && str[i+2] === 'I') {
       cnt++;
+      i++;
+    } else {
+      cnt = 0;
+    }
+    if(cnt === N) {
+      ans++;
+      cnt--;
     }
   }
 
-  console.log(cnt);
+  console.log(ans);
 }
 
 solution();
