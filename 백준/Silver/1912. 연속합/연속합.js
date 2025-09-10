184681696d235f329f4b25e07ddb19d5c5086511
Kim DongHo <61777992+kim-dongho@users.noreply.github.com>
2025-09-10 16:17:49 +0900

    [Silver II] Title: 연속합, Time: 176 ms, Memory: 17012 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -0,0 +1,25 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const splitKeyword = process.platform === "linux" ? '\n' : '\r\n';
+const fs = require('fs');
+const isTest = false;
+
+const input = fs
+  .readFileSync(isTest ? 0 : filePath, 'utf-8')
+  .toString()
+  .trim()
+  .split(splitKeyword);
+
+const N = Number(input[0]);
+const arr = input[1].split(' ').map((v) => Number(v));
+const dp = [];
+
+const solution = () => {
+	for(let i = 0; i < N; i++) {
+		if(i === 0) dp[i] = arr[i];
+		else dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
+	}
+
+	console.log(Math.max(...dp));
+}
+
+solution();
